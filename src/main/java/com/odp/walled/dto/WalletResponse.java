962cf09b0660e87d6b1e7ec079793db47a48c388
cikiners <igpemulihan743@gmail.com>
2025-04-10 19:17:13 +0700

    feat:add-user-response-in-walled

--- src/main/java/com/odp/walled/dto/WalletResponse.java
+++ src/main/java/com/odp/walled/dto/WalletResponse.java
@@ -12,4 +12,5 @@ public class WalletResponse {
     private BigDecimal balance;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+    private UserResponse user;
 }
--- src/main/java/com/odp/walled/mapper/WalletMapper.java
+++ src/main/java/com/odp/walled/mapper/WalletMapper.java
@@ -1,6 +1,8 @@
 package com.odp.walled.mapper;
 
+import com.odp.walled.dto.UserResponse;
 import com.odp.walled.dto.WalletResponse;
+import com.odp.walled.model.User;
 import com.odp.walled.model.Wallet;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -12,4 +14,6 @@ public interface WalletMapper {
     WalletResponse toResponse(Wallet wallet);
 
     List<WalletResponse> toResponseList(List<Wallet> wallets);
+
+    UserResponse toUserResponse(User user);
 }
